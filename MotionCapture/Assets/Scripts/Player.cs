b7b9a57e3ba5dd52fdb0a3ba72ed5608b50ee697
yf7890yf7890 <yf7890yf7890@users.noreply.github.com>
2019-10-24 10:50:43 +0800

    nice

---
+++ MotionCapture/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Animator ani;
+
+    private void Start()
+    {
+        ani = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.R))
+        {
+            ani.SetBool("跑步開關", true);
+        }
+        else
+        {
+            ani.SetBool("跑步開關", false);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+           ani.SetTrigger("跳舞觸發器");
+        }
+        //if (Input.GetKeyDown(KeyCode.Space)) ani.SetTrigger("跳舞觸發器");
+
+        //ani.SetBool("跑步開關", Input.GetKey(KeyCode.R));
+    }
+}
